f7ac5a8d90b08040a3bb1afd99fdf50bf28c3d59
Joshua Santiago <joshman1019@gmail.com>
2024-07-09 22:45:34 -0400

    Added a configuration based extensions list so that the list is now customizable. See extensions.json

---
+++ FileSharp/Modules/ConfigurationRetrieval.fs
@@ -0,0 +1,15 @@
+﻿module ConfigurationRetrieval
+
+open Microsoft.Extensions.Configuration
+
+type public ExtensionData = {
+    Extension: string
+    ProductName: string
+}
+
+let public GetConfigurationExtensions() =
+    let mutable extensions: ExtensionData list = []
+    ConfigurationBuilder().AddJsonFile("extensions.json", false).Build()
+    |> fun config -> config.GetSection("extensions").Bind(extensions)
+    extensions
+
--- FileSharp/Modules/ExtensionData.fs
+++ FileSharp/Modules/ExtensionData.fs
@@ -1,41 +1,12 @@
 ﻿module ExtensionData
+open ConfigurationRetrieval
 open System
 
 /// <summary>
 /// Table of common file extensions and their corresponding product or service names.
 /// </summary>
-let public ProductTable = [
-    "EXE", "Executable File";
-    "MSI", "Windows Installer Package";
-    "DOC", "Microsoft Word Document";
-    "DOCX", "Microsoft Word Open XML Document";
-    "XLS", "Microsoft Excel Spreadsheet";
-    "XLSX", "Microsoft Excel Open XML Spreadsheet";
-    "PPT", "Microsoft PowerPoint Presentation";
-    "PPTX", "Microsoft PowerPoint Open XML Presentation";
-    "PUB", "Microsoft Publisher Document";
-    "ACCDB", "Microsoft Access Database";
-    "MDB", "Microsoft Access Database";
-    "MD", "Markdown Document";
-    "JPG", "Images";
-    "PNG", "Images";
-    "GIF", "Images";
-    "WEBP", "Images";
-    "BMP", "Images";
-    "SVG", "Professional Images";
-    "ICO", "Professional Images";
-    "PDF", "PDF Documents";
-    "MP3", "Audio Files";
-    "MP4", "Video Files";
-    "ZIP", "Compressed Files";
-    "RAR", "Compressed Files";
-    "TXT", "Text Files";
-    "PS1", "PowerShell Script";
-    "PSM1", "PowerShell Module";
-    "BAT", "Batch File";
-    "CMD", "Command Script";
-    "TORRENT", "Torrent File";
-]
+let public ProductTable =
+    GetConfigurationExtensions()
 
 /// <summary>
 /// Returns the product or service name for a given file extension, or the extension itself if not found.
@@ -44,8 +15,8 @@ let public ProductTable = [
 /// <param name="normalizedFileExtension"></param>
 let public ReturnProductDirectoryName (normalizedFileExtension: string) = 
     ProductTable
-    |> List.tryFind (fun (key, _) -> key = normalizedFileExtension)
-    |> Option.map (fun (_, value) -> value)
+    |> List.tryFind (fun (item) -> item.Extension.Equals(normalizedFileExtension, StringComparison.OrdinalIgnoreCase))
+    |> Option.map (fun (item) -> item.ProductName)
     |> Option.defaultValue normalizedFileExtension
 
 /// <summary>
